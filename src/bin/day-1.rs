b09fe8216d99c03e3e0daaf906fb534a30d070e3
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2017-12-02 12:46:30 +0100

    day1 second part

--- src/bin/day-1.rs
+++ src/bin/day-1.rs
@@ -3,6 +3,9 @@ extern crate structopt;
 extern crate structopt_derive;
 use structopt::StructOpt;
 
+extern crate modulo;
+use modulo::Mod;
+
 fn compute(input: &str) -> u32 {
     let numbers: Vec<u32> = input
         .chars()
@@ -28,17 +31,50 @@ fn compute(input: &str) -> u32 {
     sum
 }
 
+fn compute2(input: &str) -> u32 {
+    let numbers: Vec<u32> = input
+        .chars()
+        .map(|x| x.to_string().parse().unwrap())
+        .collect();
+    let mut sum = 0;
+    let n = numbers.len();
+
+    // list has an even number of elements
+    assert_eq!(n % 2, 0);
+
+    for i in 0..n {
+        let prev = {
+            let delta = i as i32 - n as i32 / 2;
+            // We need the mathematical modulo while the '%' operator is the remainder
+            let j = delta.modulo(n as i32);
+            numbers[j as usize]
+        };
+
+        if numbers[i] == prev {
+            sum += numbers[i]
+        }
+    }
+
+    sum
+}
+
 #[derive(StructOpt)]
 #[structopt(name = "day-1", about = "http://adventofcode.com/2017/day/1")]
 struct Opt {
     #[structopt(help = "Input")]
     input: String,
+    #[structopt(short = "s", help = "Second part")]
+    second_part: bool,
 }
 
 fn main() {
     let opt = Opt::from_args();
+    let result = match opt.second_part {
+        true => compute2(opt.input.as_ref()),
+        false => compute(opt.input.as_ref()),
+    };
 
-    println!("{}", compute(opt.input.as_ref()));
+    println!("{}", result);
 }
 
 #[cfg(test)]
@@ -51,5 +87,14 @@ mod tests {
         assert_eq!(compute("1111"), 4);
         assert_eq!(compute("1234"), 0);
         assert_eq!(compute("91212129"), 9);
+    }
+
+    #[test]
+    fn examples2() {
+        assert_eq!(compute2("1212"), 6);
+        assert_eq!(compute2("1221"), 0);
+        assert_eq!(compute2("123425"), 4);
+        assert_eq!(compute2("123123"), 12);
+        assert_eq!(compute2("12131415"), 4);
     }
 }
